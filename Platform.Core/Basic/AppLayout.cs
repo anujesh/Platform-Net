9dddfec694c74f6af92c9a843ce82f12d1687179
Bala <bthavapalan@gmail.com>
2015-08-09 14:17:12 +1000

    Layout data builder class added

---
+++ Platform.Core/Basic/AppLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Core.Basic
+{
+    public class AppLayout
+    {
+        public string _Title { get; set; }
+        public string _Descript { get; set; }
+        public string _Keywords { get; set; }
+    }
+}
