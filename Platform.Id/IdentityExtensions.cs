9e5e4ed0aa31c3fcc0047d2a5cde148b93c15a88
Bala <bthavapalan@gmail.com>
2016-01-30 09:39:20 +1100

    Adding platform.id files

---
+++ Platform.Id/IdentityExtensions.cs
@@ -0,0 +1,29 @@
+﻿//using Microsoft.AspNet.Identity;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Platform.Id
+//{
+//    public static class IdentityExtensions
+//    {
+//        public static async Task<ApplicationUser> FindByNameOrEmailAsync
+//            (this UserManager<ApplicationUser> userManager, string usernameOrEmail, string password)
+//        {
+//            var username = usernameOrEmail;
+//            if (usernameOrEmail.Contains("@"))
+//            {
+//                var userForEmail = await userManager.FindByEmailAsync(usernameOrEmail);
+//                if (userForEmail != null)
+//                {
+//                    username = userForEmail.UserName;
+//                }
+//            }
+//            return await userManager.FindAsync(username, password);
+//        }
+
+
+//    }
+//}
---
+++ Platform.Id/UserToDo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Platform.Id
+{
+    public class UserToDo
+    {
+        public int UserId { get; set; }
+
+        public UserToDoType Type { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public enum UserToDoType
+    {
+        ChangeDisplayName,
+
+        ChangeUserName,
+
+        ChangePassword,
+
+        AgreeTandC,
+
+        UploadProfileImage
+    }
+
+    public enum UserToDoBlockType
+    {
+        Strict,
+
+        Suggested
+    }
+
+
+    public class ExternalTokens
+    {
+        public int RefId { get; set; }
+
+        //80
+        public string Token { get; set; }
+
+        public TokenActions TokenAction { get; set; }
+
+        public DateTime ExpireDate { get; set; }
+
+        public int UserId { get; set; }
+
+
+    }
+
+    public enum TokenCheckResponse
+    {
+        Active,
+
+        NotFound,
+
+        Expired,
+
+        InActive
+    }
+
+    public interface ITokenService
+    {
+        ExternalTokens GetExternalToken(string emailGuid, out TokenCheckResponse tokenCheckResponse);
+    }
+
+    public class TokenService : ITokenService
+    {
+        public ExternalTokens GetExternalToken(string emailGuid, out TokenCheckResponse tokenCheckResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITokenRepository
+    {
+
+    }
+
+    public class TokenRepository : ITokenRepository
+    {
+
+    }
+
+    public enum TokenActions
+    {
+        ValidateEmail
+    }
+}
