d30c7f80490b3c4d810197ab5f4186651e180ac3
Bala <bthavapalan@gmail.com>
2016-01-30 09:16:41 +1100

    adding Platform.Id files to Platform project

---
+++ Platform.Id/IdentityModels.cs
@@ -0,0 +1,82 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
+
+////namespace Platform.Id.MySQL
+////{
+////    public class CustomUserRole : IdentityUserRole<int> { }
+////    public class CustomUserClaim : IdentityUserClaim<int> { }
+////    public class CustomUserLogin : IdentityUserLogin<int> { }
+
+////    public class CustomRole : IdentityRole<int, CustomUserRole>
+////    {
+////        public CustomRole() { }
+////        public CustomRole(string name) { Name = name; }
+////    }
+
+////    public class CustomUserStore : UserStore<ApplicationUser, CustomRole, int,
+////        CustomUserLogin, CustomUserRole, CustomUserClaim>
+////    {
+////        public CustomUserStore(ApplicationDbContext context)
+////            : base(context)
+////        {
+////        }
+////    }
+
+////    public class CustomRoleStore : RoleStore<CustomRole, int, CustomUserRole>
+////    {
+////        public CustomRoleStore(ApplicationDbContext context)
+////            : base(context)
+////        {
+////        }
+////    } 
+////}
+
+
+//// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+////public class ApplicationUser : IdentityUser<int, CustomUserLogin, CustomUserRole, CustomUserClaim>
+
+//using Microsoft.AspNet.Identity;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Claims;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Platform.Id { 
+//public class ApplicationUser : IdentityUser
+//{
+//    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+//    {
+//        // Note the authenticationType must match the one defined in
+//        // CookieAuthenticationOptions.AuthenticationType 
+//        var userIdentity = await manager.CreateIdentityAsync(
+//            this, DefaultAuthenticationTypes.ApplicationCookie);
+//        // Add custom user claims here 
+//        return userIdentity;
+//    }
+//}
+
+//public class ApplicationDbContext : MySQLDatabase
+//{
+//    public ApplicationDbContext(string connectionName)
+//        : base(connectionName)
+//    {
+//    }
+
+//    public static ApplicationDbContext Create()
+//    {
+//        return new ApplicationDbContext("MySqlDB");
+//    }
+//}
+//}
+
+
+
+
+
+////http://www.asp.net/identity/overview/extensibility/change-primary-key-for-users-in-aspnet-identity
---
+++ Platform.Id/Model/IdentityExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace Platform.Id.Model
+{
+    public static class IdentityExtensions
+    {
+        public static async Task<ApplicationUser> FindByNameOrEmailAsync
+            (this UserManager<ApplicationUser> userManager, string usernameOrEmail, string password)
+        {
+            var username = usernameOrEmail;
+            if (usernameOrEmail.Contains("@"))
+            {
+                var userForEmail = await userManager.FindByEmailAsync(usernameOrEmail);
+                if (userForEmail != null)
+                {
+                    username = userForEmail.UserName;
+                }
+            }
+            return await userManager.FindAsync(username, password);
+        }
+    }
+}
