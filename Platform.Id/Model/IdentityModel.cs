8176b46ad6b069eb8d5511611f6f4284faab136e
Bala <bthavapalan@gmail.com>
2016-01-31 12:44:03 +1100

    Missing reference mapped

--- Platform.Id/Model/IdentityModel.cs
+++ Platform.Id/Model/IdentityModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNet.Identity;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
 
 namespace Platform.Id.Model
 {
