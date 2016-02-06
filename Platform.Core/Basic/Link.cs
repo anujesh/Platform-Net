4a09849aab553f6b8e7757b7e7493b713ecf200b
Bala <bthavapalan@gmail.com>
2016-02-06 19:01:20 +1100

    Href model added to core project

--- Platform.Core/Basic/Link.cs
+++ Platform.Core/Basic/Link.cs
@@ -1,4 +1,6 @@
-﻿namespace Platform.Core.Basic
+﻿using System.Collections.Generic;
+
+namespace Platform.Core.Basic
 {
     
     //ula_web_bas_link_master
@@ -25,6 +27,15 @@
 
     public class rpLinks : ResponseItem<Links>
     {
+    }
+
+
+    public class Href
+    {
+        public string Link { get; set; }
+        public string Text { get; set; }
+        public string Icon { get; set; }
+        public List<Href> Kids { get; set; }
     }
 
 }
